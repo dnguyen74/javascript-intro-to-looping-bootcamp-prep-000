661c7f0490f79e265ae47637b5fd904d3170edb0
Dat Nguyen <dnguyen74@gmail.com>
2018-02-23 04:38:55 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,15 +1,15 @@
-function forLoop() {
-  const array = [];
+function forLoop(arrayS) {
+  //const array = [];
   //const t = Math.floor(Math.random() * 10)
 
   for (let i = 0; i < 25; i++) {
 	  if (i===1) {
-	    array.push("I am 1 strange loop.");
+	    arrayS.push("I am 1 strange loop.");
     }
     else{
-      array.push("I am ${i} strange loops.");
+      arrayS.push("I am ${i} strange loops.");
     }
   }
 
-  return array;
+  return arrayS;
 }
