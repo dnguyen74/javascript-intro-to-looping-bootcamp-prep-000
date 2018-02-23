d0515a8110628efd338a571312a231a77cf1e0bd
Dat Nguyen <dnguyen74@gmail.com>
2018-02-23 04:30:39 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,13 @@
+function makeArray() {
+  const array = []
+  const t = Math.floor(Math.random() * 10)
+
+  for (let i = 0; i < 25; i++) {
+	if (i===0) {
+	
+}
+    array.push("I am a strange loop.")
+  }
+
+  return [array, t]
+}
